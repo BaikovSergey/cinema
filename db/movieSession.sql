8d728aeb69f5e320e93bc3ca4a00232351f9a8f8
Sergey Baikov <rayleonard9@gmail.com>
2020-07-24 10:44:45 +0300

    movie session table: add unique constraint to seats

--- db/movieSession.sql
+++ db/movieSession.sql
@@ -4,13 +4,15 @@ create table moviesessions
   id           serial      not null
     constraint halls_pkey
     primary key,
-  data         date        not null,
+  data         varchar(20) not null,
   film_name    varchar(50) not null,
   hall_id      integer     not null,
   session_time varchar(50) not null,
-  seats        text
+  seats        text        not null
 );
 
 alter table moviesessions
   owner to postgres;
 
+create unique index moviesessions_seats_uindex
+  on moviesessions (seats);
